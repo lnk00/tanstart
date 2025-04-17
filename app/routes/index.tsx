46b24a49c0f6f472bcdc3f166b0536ccce6f0efc
Damien Dumontet <damiendumontet@192.168.68.105>
2025-04-17 17:09:28 -0300

    add origin ui button

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/" as never)({
@@ -6,8 +7,11 @@ export const Route = createFileRoute("/" as never)({
 
 function RouteComponent() {
 	return (
-		<div className="h-screen w-screen flex items-center justify-center">
+		<div className="h-screen w-screen flex flex-col items-center justify-center gap-8">
 			<div className="text-9xl">Hello world!</div>
+			<Button className="rounded-full" size={"lg"}>
+				Button
+			</Button>
 		</div>
 	);
 }
